ff07069143f63553452a7fa21b5d74b77c551f42
sumin_Kim_1202 <pizz201207@naver.com>
2019-10-04 00:30:14 +0900

    first_envision_character
    캐릭터 리소스

---
+++ character_runs.py
@@ -0,0 +1,11 @@
+from pico2d import *
+open_canvas()
+
+Slime_P = load_image('Pink_Slime.png')
+Slime_G = load_image('Green_Slime.png')
+Slime_B = load_image('Blue_Slime.png')
+
+x = 0
+
+close_canvas()
+
