23d85d8ca474649ef022207b6ab33e1b925c5475
sumin_Kim_1202 <pizz201207@naver.com>
2019-10-05 01:27:52 +0900

    캐릭터 투명처리

--- character_runs.py
+++ character_runs.py
@@ -1,11 +1,21 @@
 from pico2d import *
 open_canvas()
 
-Slime_P = load_image('Pink_Slime.png')
+Slime_P = load_image('run_Pink_Slime.png')
 Slime_G = load_image('Green_Slime.png')
 Slime_B = load_image('Blue_Slime.png')
 
+isDone = True
+
 x = 0
+frame = 0
+while True:
+    clear_canvas()
+    while True:
+        Slime_P.draw_now(100, 90)
+        update_canvas()
+    delay(0.01)
+    get_events()
 
 close_canvas()
 
