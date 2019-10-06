f517861c1465889ddf4ce466f4021d04eb2d0495
sumin_Kim_1202 <pizz201207@naver.com>
2019-10-07 00:50:37 +0900

    캐릭터프레임맞추기

--- character_runs.py
+++ character_runs.py
@@ -7,7 +7,7 @@ open_canvas(Back_Width, Back_Height)
 Slime_P = load_image('run_Pink_Slime.png')
 Slime_G = load_image('Green_Slime.png')
 Slime_B = load_image('Blue_Slime.png')
-BackG = load_image('background.png')
+BackG = load_image('vv.png')
 
 running = True
 
@@ -15,10 +15,10 @@ frame = 0
 while running:
     clear_canvas()
     BackG.draw(Back_Width // 2, Back_Height // 2)
-    Slime_P.clip_draw(frame * 100, 0, 100, 100, 200, 200)
+    Slime_P.clip_draw(frame * 137, 0, 130, 130, 90, 200)
+    frame = (frame + 1) % 4
     update_canvas()
-    frame = (frame + 1) % 8
-    delay(0.02)
+    delay(0.1)
     get_events()
 
 close_canvas()
