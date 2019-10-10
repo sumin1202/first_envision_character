8cef00ea1252f58d4350932702919fdeaba7ad46
sumin_Kim_1202 <pizz201207@naver.com>
2019-10-11 01:54:04 +0900

    장애물/바닥

--- character_runs.py
+++ character_runs.py
@@ -1,6 +1,6 @@
 from pico2d import *
 
-Back_Width, Back_Height = 960, 540
+Back_Width, Back_Height = 1400, 780
 
 open_canvas(Back_Width, Back_Height)
 
@@ -8,6 +8,8 @@ Slime_P = load_image('run_Pink_Slime.png')
 Slime_G = load_image('Green_Slime.png')
 Slime_B = load_image('Blue_Slime.png')
 BackG = load_image('vv.png')
+grass = load_image('grass.png')
+block = load_image('bb.png')
 
 running = True
 
@@ -15,11 +17,12 @@ frame = 0
 while running:
     clear_canvas()
     BackG.draw(Back_Width // 2, Back_Height // 2)
-    Slime_P.clip_draw(frame * 137, 0, 130, 130, 90, 200)
+    Slime_P.clip_draw(frame * 137, 0, 130, 130, 250, 200)
+    grass.draw(0, 100)
+    block.draw(800, 100)
     frame = (frame + 1) % 4
     update_canvas()
     delay(0.1)
     get_events()
 
 close_canvas()
-
