b1309e0801d62c3f7c4d5dbf8d7f307507fb149a
sumin_Kim_1202 <pizz201207@naver.com>
2019-10-13 03:05:46 +0900

    장애물파괴모션-1

--- character_runs.py
+++ character_runs.py
@@ -7,19 +7,27 @@ open_canvas(Back_Width, Back_Height)
 Slime_P = load_image('run_Pink_Slime.png')
 Slime_G = load_image('Green_Slime.png')
 Slime_B = load_image('Blue_Slime.png')
+jump_P = load_image('jump_Pink_Slime.png')
 BackG = load_image('vv.png')
-grass = load_image('grass.png')
 block = load_image('bb.png')
 
 running = True
-
 frame = 0
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
+            jump_P.clip_draw(frame * 100, 0, 110, 110, 300, 300)
+
+
 while running:
     clear_canvas()
     BackG.draw(Back_Width // 2, Back_Height // 2)
-    Slime_P.clip_draw(frame * 137, 0, 130, 130, 250, 200)
-    grass.draw(0, 100)
-    block.draw(800, 100)
+    Slime_P.clip_draw(frame * 100, 0, 110, 110, 300, 300)
     frame = (frame + 1) % 4
     update_canvas()
     delay(0.1)
