abb49198d60198d66bcf3a58c624fb4f6c240292
sumin_Kim_1202 <pizz201207@naver.com>
2019-10-29 23:28:26 +0900

    장애물 충돌모션

--- main_state.py
+++ main_state.py
@@ -12,53 +12,88 @@ name = "MainState"
 Back_Width, Back_Height = 1400, 780
 character = None
 background = None
+attack_ch = None
+block = None
 font = None
 
-#Character Event
-
+check = 0
 
 class Background:
     def __init__(self):
         self.x, self.y = 700, 390
-        self.x1, self.y1 = 2100, 390
+        self.x1, self.x2 = 2100, 3500
         self.image = load_image('b1.png')
 
     def update(self):
-        self.x -= 2
-        self.x1 -= 2
+        self.x -= 10
+        self.x1 -= 10
+        self.x2 -= 10
 
     def draw(self):
         self.image.draw(self.x, self.y)
-        self.image.draw(self.x1, self.y1)
+        self.image.draw(self.x1, self.y)
+        self.image.draw(self.x2, self.y)
 
 class Character:
     def __init__(self):
+        global check
         self.frame = 0
+        self.cx, self.cy = 270, 190
         self.image = load_image('run_Pink_Slime.png')
+        self.dir = 1
+
+    def update(self):
+        global check
+        self.frame = (self.frame + 1) % 4
+
+    def draw(self):
+        global check
+        self.image.clip_draw(self.frame * 100, 0, 105, 110, self.cx, self.cy)
+        delay(0.1)
+
+class Attack_Slime:
+    def __init__(self):
+        self.frame = 0
+        self.cx, self.cy = 270, 190
+        self.image = load_image('neum.png')
         self.dir = 1
 
     def update(self):
         self.frame = (self.frame + 1) % 4
 
     def draw(self):
-        self.image.clip_draw(self.frame * 100, 0, 110, 110, 270, 190)
+        self.image.draw(self.cx, self.cy)
         delay(0.1)
 
-class Attack_Character:
+class Block:
     def __init__(self):
-        self.frame = 0
-        self.image2 = load_image('jump_Pink_Slime')
+        self.bx, self.by = 1000, 100
+        self.bx2, self.by2 = 1600, 370
+        self.image = load_image('bb.png')
+
+    def update(self):
+        self.bx -= 10
+        self.bx2 -= 10
+        pass
+
+    def draw(self):
+        self.image.draw(self.bx, self.by)
+        self.image.draw(self.bx2, self.by2)
 
 def enter():
-    global character, background
+    global character, background, attack_ch, block
     character = Character()
+    attack_ch = Attack_Slime()
     background = Background()
+    block = Block()
     pass
 
 
 def exit():
-    global character, background
+    global character, background, attack_ch
+    del(attack_ch)
     del(character)
+    del(block)
     del(background)
     pass
 
@@ -72,6 +107,7 @@ def resume():
 
 
 def handle_events():
+    global check
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -79,19 +115,27 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.change_state(title_state)
         elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
-            pass
+            check = 1
     pass
 
-
 def update():
+    global check
     character.update()
+    attack_ch.update()
+    block.update()
     background.update()
     pass
 
 
 def draw():
+    global check
     clear_canvas()
     background.draw()
-    character.draw()
+    block.draw()
+    if check == 1:
+        attack_ch.draw()
+        check = 0
+    else:
+        character.draw()
     update_canvas()
     pass
