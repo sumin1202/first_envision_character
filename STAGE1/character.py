5222032bb88165526c0944a686f1270ced0bf7d8
sumin_Kim_1202 <pizz201207@naver.com>
2019-11-07 00:32:31 +0900

    state구현

--- STAGE1/character.py
+++ STAGE1/character.py
@@ -1,4 +1,73 @@
 from pico2d import*
 
-# Chararcter Event
-RUN_SLIME, JUMP_SLIME = range(2)
+# character Event
+ATTACK_SLIME, JUMP_SLIME, CHANGE_SLIME, SLEEP_TIMER = range(4)
+
+key_event_table = {
+    (SDLK_DOWN, SDLK_q): CHANGE_SLIME,
+    (SDL_KEYDOWN, SDLK_SPACE): JUMP_SLIME
+}
+
+# IDLE 에서 Timer_out 되었을 때
+class SleepState:
+    @staticmethod
+    def enter(chararcter, event):
+        chararcter.frame = 0
+
+    @staticmethod
+    def exit(character, event):
+        pass
+
+    @staticmethod
+    def do(character):
+        character.frame = (character.frame + 1) % 4
+        character.cx, character.cy = 270, 190
+
+    @staticmethod
+    def draw(character):
+        if character.dir == 1:
+            character.image.clip_draw(character.frame * 100, 0, 105, 110, character.cx, character.cy)
+
+# 아무것도 안하고 있는 상태
+class IdleState:
+    @staticmethod
+    def enter(character, event):
+        pass
+
+    @staticmethod
+    def exit(character, event):
+        pass
+
+    @staticmethod
+    def do(character):
+        character.frame = (character.frame + 1) % 4
+        character.timer -= 1
+
+    @staticmethod
+    def draw(character):
+        if character.dir == 1:
+            character.image.clip_draw(character.frame * 100, 0, 105, 110, character.cx, character.cy)
+
+
+class RunState:
+    # staticmethod 란 정적메소드와 비슷한 것, 인스턴스 없이 호출이 가능(cls 를 넘겨주지 않아도 됨)
+    @staticmethod
+    def enter(character, event):
+        if event == JUMP_SLIME:
+            pass
+
+    @staticmethod
+    def exit(character, event):
+        pass
+
+    @staticmethod
+    def do(character):
+        character.frame = (character.frame + 1) % 4
+
+    @staticmethod
+    def draw(chararcter):
+        pass
+
+next_state_table = {
+    IdleState: {}
+}
