834f4111b208d8d4cf1cb88115b663f27e21ae6a
sumin_Kim_1202 <pizz201207@naver.com>
2019-11-17 15:44:42 +0900

    start_state.py

---
+++ STAGE1/start_state.py
@@ -0,0 +1,49 @@
+from pico2d import *
+
+
+name = "StartState"
+image = None
+logo_time = 0.0
+
+
+def enter():
+    global image
+    image = load_image('kpu_credit.png')
+
+
+def exit():
+    global image
+    del(image)
+
+
+from Before import title_state, game_framework
+
+
+def update():
+    global logo_time
+
+    if (logo_time > 1.0):
+        logo_time = 0
+        #game_framework.quit()
+        game_framework.change_state(title_state)
+    delay(0.01)
+    logo_time += 0.01
+
+
+
+def draw():
+    global image
+    clear_canvas()
+    image.draw(700, 390)
+    update_canvas()
+
+
+def handle_events():
+    events = get_events()
+    pass
+
+
+def pause(): pass
+
+
+def resume(): pass
